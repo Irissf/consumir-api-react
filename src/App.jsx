108d135deb45ae38b9309aaed5c4a5c87d626265
Irissf <iris.seijo@gmail.com>
2022-04-06 11:57:47 +0200

    Siguiente paso react

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {React,useState,useEffect} from 'react'
+
+const App = () => {
+
+    const[contador,setContador] = useState(0)
+
+    //Se ejecuta al renderizar una web, por lo que al aumentar el contador
+    //se renderiza de nuevo
+    useEffect(()=>{
+        console.log("renderizado")
+    },[]);
+
+    return (
+        <div className='container'>
+            <h1>Rick and morty</h1>
+            <h3>{contador}</h3>
+            <button onClick={() => setContador(contador + 1)}>agregar</button>
+        </div>
+    )
+};
+
+export default App
