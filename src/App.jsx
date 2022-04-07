4d3696cb27076c6d1cf48faccb145758eef0cfdf
Irissf <iris.seijo@gmail.com>
2022-04-07 15:11:37 +0200

    hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import {React,useState,useEffect} from 'react'
+import Formulario from './components/Formulario';
 
 const App = () => {
 
-    const[contador,setContador] = useState(0)
-
-    //Se ejecuta al renderizar una web, por lo que al aumentar el contador
-    //se renderiza de nuevo
-    useEffect(()=>{
-        console.log("renderizado "+ contador)
-    },[contador]);
-
     return (
         <div className='container'>
-            <h1>Rick and morty</h1>
-            <h3>{contador}</h3>
-            <button onClick={() => setContador(contador + 1)}>agregar</button>
+            <h1>App Rick and Morty</h1>
+           <Formulario></Formulario>
         </div>
     )
 };
---
+++ src/hooks/useFormulario.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+const useFormulario = (initialState={}) => {
+
+    const [inputs, setImputs] = useState();
+
+    return (
+        <>
+        <div className='container'>
+     
+        </div>
+        
+    </>
+  )
+}
+
+export default useFormulario
